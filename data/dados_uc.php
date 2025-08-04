7454cd547618da807209114faaad018448815cb4
Juliano Silva <57194781+JulianoSilva05@users.noreply.github.com>
2025-08-03 23:42:25 -0300

    Dados Usados para simular UC
    Dados usados para simular a criação de nova turma baseado nas UCs criadas no documento

---
+++ data/dados_uc.php
@@ -0,0 +1,45 @@
+<?php
+
+$unidades_curriculares = [
+    [
+        'id' => 1,
+        'descricao' => 'Lógica de Programação',
+        'presencial_ch' => 80,
+        'presencial_qa' => 40,
+        'presencial_qd' => 20,
+        'ead_ch' => 0,
+        'ead_qa' => 0,
+        'ead_qd' => 0,
+    ],
+    [
+        'id' => 2,
+        'descricao' => 'Modelagem de Sistemas',
+        'presencial_ch' => 60,
+        'presencial_qa' => 30,
+        'presencial_qd' => 15,
+        'ead_ch' => 20,
+        'ead_qa' => 10,
+        'ead_qd' => 5,
+    ],
+    [
+        'id' => 3,
+        'descricao' => 'Banco de Dados',
+        'presencial_ch' => 100,
+        'presencial_qa' => 50,
+        'presencial_qd' => 25,
+        'ead_ch' => 0,
+        'ead_qa' => 0,
+        'ead_qd' => 0,
+    ],
+    [
+        'id' => 4,
+        'descricao' => 'Desenvolvimento Web Frontend',
+        'presencial_ch' => 120,
+        'presencial_qa' => 60,
+        'presencial_qd' => 30,
+        'ead_ch' => 40,
+        'ead_qa' => 20,
+        'ead_qd' => 10,
+    ],
+];
+?>
