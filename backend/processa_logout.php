d3ca9534da40bc238df1b7ca02743edd5523bf06
Lorrany Marim <lorranybatistaamorim@gmail.com>
2026-04-10 15:50:35 -0300

    Destruição Ativa de Sessão no Logout

--- backend/processa_logout.php
+++ backend/processa_logout.php
@@ -1,5 +1,8 @@
 <?php
-// backend/processa_logout.php
+session_start();
+session_unset();
+session_destroy();
+
 if (isset($_COOKIE['session_token'])) {
     // Invalida o cookie no navegador definindo validade no passado
     setcookie('session_token', '', time() - 3600, '/'); 
